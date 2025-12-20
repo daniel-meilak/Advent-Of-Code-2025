c082d24ee11c34138cee99ae722d8701255e0213
Daniel Meilak <daniel.meilak@egym.com>
2025-12-20 16:38:18 +0100

    formatting

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -3,29 +3,26 @@ advent_of_code::solution!(2);
 fn check_pattern(id: &str, pattern_sizes: &[usize]) -> bool {
     'next_pattern: for i in pattern_sizes {
         let section_size = id.len() / i;
-        
+
         if !id.len().is_multiple_of(*i) {
             continue;
         }
 
         let section = &id[0..section_size];
         for j in 1..*i {
-            if &id[(section_size*j)..(section_size*(j+1))] != section {
+            if &id[(section_size * j)..(section_size * (j + 1))] != section {
                 continue 'next_pattern;
             }
         }
 
         return true;
     }
-    
+
     false
 }
 
 fn find_invalid_ids(input: &str, pattern_sizes: &[usize]) -> Option<u64> {
-    let ranges = input
-        .trim()
-        .split(',')
-        .filter_map(|s| s.split_once('-'));
+    let ranges = input.trim().split(',').filter_map(|s| s.split_once('-'));
 
     let mut sum = 0;
     for (start, end) in ranges {
@@ -44,7 +41,7 @@ pub fn part_one(input: &str) -> Option<u64> {
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    find_invalid_ids(input, &[2,3,4,5,6,7])
+    find_invalid_ids(input, &[2, 3, 4, 5, 6, 7])
 }
 
 #[cfg(test)]
