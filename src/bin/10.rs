59e0ec78d614787cbf7f406eca5e7bb1bedb0949
Daniel Meilak <daniel.meilak@egym.com>
2025-12-30 23:36:09 +0100

    Day 10 without z3

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -1,130 +1,148 @@
-use std::collections::{HashSet, VecDeque};
+use std::collections::{HashMap, HashSet};
 
-use regex::Regex;
-use z3::{Optimize, SatResult, ast::Int};
+use itertools::Itertools;
 
+// Code translated from python impl by u/4HbQ
 advent_of_code::solution!(10);
 
-type Instruction = (Vec<usize>, Vec<Vec<usize>>, Vec<usize>);
+type Machine = (Vec<bool>, Vec<HashSet<usize>>, Vec<isize>);
 
-fn instruction_parser(input: &str) -> Option<Vec<Instruction>> {
-    let light_re = Regex::new(r"\[(.*)\]").ok()?;
-    let button_re = Regex::new(r"\(([\d,]*)\)").ok()?;
-    let joltage_re = Regex::new(r"\{(.*)\}").ok()?;
-
+fn parse(input: &str) -> Vec<Machine> {
     let mut machines = Vec::new();
     for line in input.lines() {
-        let light: Vec<usize> = light_re
-            .captures(line)
-            .and_then(|c| c.get(1))
-            .map(|m| m.as_str().chars().map(|c| if c == '.' {0} else {1}).collect())?;
-
-        let buttons: Vec<Vec<usize>> = button_re
-            .captures_iter(line)
-            .map(|cap| {
-                cap[1]
-                    .split(',')
-                    .filter_map(|s| s.parse().ok())
-                    .collect()
-            })
+        let temp: Vec<_> = line
+            .split_whitespace()
+            .map(|s| &s[1..s.len() - 1])
             .collect();
 
-        let joltage: Vec<usize> = joltage_re.captures(line).and_then(|c| c.get(1)).map(|m| {
-            m.as_str()
-                .split(',')
-                .filter_map(|s| s.parse().ok())
-                .collect()
-        })?;
+        let lights: Vec<_> = temp[0].chars().map(|c| c == '#').collect();
+        let joltages: Vec<isize> = temp[temp.len() - 1]
+            .split(',')
+            .filter_map(|s| s.parse().ok())
+            .collect();
+        let buttons: Vec<HashSet<usize>> = temp[1..temp.len() - 1]
+            .iter()
+            .map(|&s| s.split(',').filter_map(|s| s.parse().ok()).collect())
+            .collect();
 
-        machines.push((light, buttons, joltage));
+        machines.push((lights, buttons, joltages));
     }
 
-    Some(machines)
+    machines
 }
 
-fn solver(joltages: Vec<usize>, button_schematics: Vec<Vec<usize>>) -> Option<u64> {
-    let opt = Optimize::new();
-    let total_presses = Int::fresh_const("total_presses");
-
-    let button_presses: Vec<Int> = (0..button_schematics.len())
-        .map(|i| Int::fresh_const(&format!("button_{i}")))
-        .collect();
-
-    button_presses.iter().for_each(|b| opt.assert(&b.ge(0)));
-
-    for (button_index, &target_presses) in joltages.iter().enumerate() {
-        let mut terms = Vec::new();
-
-        for (i, button_schematic) in button_schematics.iter().enumerate() {
-            if button_schematic.contains(&button_index) {
-                terms.push(button_presses[i].clone());
-            }
-        }
-
-        let sum = Int::add(&terms.iter().collect::<Vec<&Int>>());
-        opt.assert(&sum.eq(Int::from_u64(target_presses as u64)));
-    }
-
-    opt.assert(&total_presses.eq(Int::add(&button_presses)));
-    opt.minimize(&total_presses);
-
-    match opt.check(&[]) {
-        SatResult::Sat => {
-            let model = opt.get_model()?;
-            model.eval(&total_presses, true).and_then(|t| t.as_u64())
-        }
-        _ => None,
-    }
+fn powerset(set: &[usize]) -> impl Iterator<Item = Vec<usize>> {
+    (0..=set.len()).flat_map(move |r| set.iter().cloned().combinations(r))
 }
 
-fn bfs(target: Vec<usize>, button_schematics: Vec<Vec<usize>>) -> usize {
-    let mut visited = HashSet::new();
-    let mut queue = VecDeque::from([(vec![0;target.len()], 0)]);
+fn opt(
+    joltages: &[isize],
+    options: &HashMap<Vec<bool>, Vec<Vec<usize>>>,
+    output: &HashMap<Vec<usize>, Vec<usize>>,
+) -> usize {
+    if let Some(&min) = joltages.iter().min()
+        && min < 0
+    {
+        return usize::MAX;
+    }
 
-    while let Some(current) = queue.pop_front() {
-        if visited.contains(&current.0) {
-            continue;
-        } else {
-            visited.insert(current.0.clone());
-        }
-        
-        if current.0 == target {
-            return current.1;
-        }
+    if joltages.iter().sum::<isize>() == 0 {
+        return 0;
+    }
 
-        for button_group in &button_schematics {
-            let mut new = current.0.clone();
-            for &button in button_group {
-                new[button] ^= 1;
-            }
+    let mut answer = usize::MAX;
+    let parity: Vec<_> = joltages.iter().map(|j| j % 2 != 0).collect();
 
-            queue.push_back((new, current.1 + 1));
+    if let Some(combinations) = options.get(&parity) {
+        for pressed in combinations {
+            let remain: Vec<_> = joltages
+                .iter()
+                .zip(output[pressed].iter())
+                .map(|(j, s)| (j - *s as isize).div_euclid(2))
+                .collect();
+
+            answer = answer.min(
+                opt(&remain, options, output)
+                    .saturating_mul(2)
+                    .saturating_add(pressed.len()),
+            );
         }
     }
 
-    0
+    answer
 }
 
 pub fn part_one(input: &str) -> Option<usize> {
-    let instructions = instruction_parser(input)?;
+    let machines = parse(input);
 
-    let mut button_presses = 0;
-    for (lights, buttons, _) in instructions {
-        button_presses += bfs(lights, buttons)
+    let mut sum = 0;
+    for (lights, buttons, joltages) in machines {
+        let mut options = HashMap::new();
+        let mut output = HashMap::new();
+
+        for pressed in powerset(&(0..buttons.len()).collect::<Vec<_>>()) {
+            let mut supply = Vec::new();
+            for j in 0..joltages.len() {
+                let mut count = 0;
+                for button in &pressed {
+                    if buttons[*button].contains(&j) {
+                        count += 1;
+                    }
+                }
+                supply.push(count);
+            }
+
+            let parity: Vec<_> = supply.iter().map(|j| j % 2 != 0).collect();
+
+            options
+                .entry(parity)
+                .or_insert(Vec::new())
+                .push(pressed.clone());
+            output.insert(pressed, supply);
+        }
+
+        sum += options
+            .get(&lights)
+            .map(|v| v.iter().map(|l| l.len()).min().unwrap_or(0))
+            .unwrap_or(0);
     }
 
-    Some(button_presses)
+    Some(sum)
 }
 
-pub fn part_two(input: &str) -> Option<u64> {
-    let instructions = instruction_parser(input)?;
+pub fn part_two(input: &str) -> Option<usize> {
+    let machines = parse(input);
 
-    let mut button_presses = 0;
-    for (_, buttons, joltages) in instructions {
-        button_presses += solver(joltages, buttons)?;
+    let mut sum = 0;
+    for (_, buttons, joltages) in machines {
+        let mut options = HashMap::new();
+        let mut output = HashMap::new();
+
+        for pressed in powerset(&(0..buttons.len()).collect::<Vec<_>>()) {
+            let mut supply = Vec::new();
+            for j in 0..joltages.len() {
+                let mut count = 0;
+                for button in &pressed {
+                    if buttons[*button].contains(&j) {
+                        count += 1;
+                    }
+                }
+                supply.push(count);
+            }
+
+            let parity: Vec<_> = supply.iter().map(|j| j % 2 != 0).collect();
+
+            options
+                .entry(parity)
+                .or_insert(Vec::new())
+                .push(pressed.clone());
+            output.insert(pressed, supply);
+        }
+
+        sum += opt(&joltages, &options, &output);
     }
 
-    Some(button_presses)
+    Some(sum)
 }
 
 #[cfg(test)]
